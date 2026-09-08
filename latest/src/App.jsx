0f99b0c31a8e9da52354327e1ed160ebafeea10a
yukioarthur <141744810+yukioarthur@users.noreply.github.com>
2026-09-08 20:34:39 -0300

    Icon variable + texts

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -1,9 +1,36 @@
+import Reactlogo from './assets/react-logo-xs.png'
+// const content = [
 
+// ]
 function App() {
   return(
- <>
-  Oi
- </>
+    <div>
+       <header>
+        <img src={Reactlogo} alt="React logo" />
+        <div>
+          <h1>React.js</h1>
+          <p>i.e., using the React library for rendering the UI</p>
+        </div>
+      </header>
+
+       <div id="tabs">
+        <menu>
+          <button>Why React?</button>
+          <button>Core Features</button>
+          <button>Related Resources</button>
+        </menu>
+
+        <div id="tab-content">
+          <ul>
+            <li>React is extremely popular</li>
+            <li>It makes building complex, interactive UIs a breeze</li>
+            <li>It's powerful & flexible</li>
+            <li>It has a very active and versatile ecosystem</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    
  )
 }
 
